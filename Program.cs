73f61792a18d11fb3f49f92af184134e2b939258
Tiandre Turner <tiandre.turner@gmail.com>
2019-08-28 11:27:33 -0400

    updated files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PracticeGH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! I am just testing on how to upload this to GitHub");
+        }
+    }
+}
